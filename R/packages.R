41f80ed7927443e3d522719d789af2e2dc833b81
MilouArts <70600687+MilouArts@users.noreply.github.com>
2021-09-30 16:11:33 +0200

    Create packages.R

---
+++ R/packages.R
@@ -0,0 +1,75 @@
+"packages.R
+
+
+Goal:  install and load all packages
+
+Written by:
+Milou Arts, NIOZ, NL, 2019
+
+List of alterations:
+
+
+"
+# Specify your packages
+package_list <- c(
+  "textclean",
+  "rmarkdown",
+  "knitr",
+  "kableExtra",
+  "tictoc",
+  "expss",
+  "vegan",
+  "stringi",
+  "psych",
+  "nortest",
+  "binom",
+  "epitools",
+  "car",
+  "ape",
+  "wesanderson",
+  "RColorBrewer",
+  "data.table",
+  "DescTools",
+  "broom",
+  "readxl",
+  "multcomp",
+  "summarytools",
+  "scales",
+  "reshape",
+  "reshape2",
+  "cluster",
+  "ggfortify",
+  "rfPermute",
+  "plyr",
+  "tidyverse",
+  "tibble",
+  "dplyr",
+  "svglite",
+  "dunn.test",
+  "UpSetR",
+  "gridExtra",
+  "grid",
+  "ggpubr",
+  "rstatix",
+  "ComplexUpset",
+  "cowplot",
+  "scatterplot3d",
+  "pdftools",
+  "png",
+  "magick",
+  "devtools")
+
+
+
+
+
+
+# not_installed <- package_list[!(package_list %in% installed.packages()[ , "Package"])]    # Extract not installed packages
+# if(length(not_installed)) install.packages(not_installed)                               # Install not installed packages
+# if(length(not_installed)>1){
+#   print(paste(length(not_installed), "packages had to be installed."))                    # Print information to console if packages had to be installed.
+# }
+
+#unpack all packages to use
+lapply(package_list, library, character.only = TRUE)
+
