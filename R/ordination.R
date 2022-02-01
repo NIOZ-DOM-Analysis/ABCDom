9026fb6efbb46a86dadf9b9c19a9e34e7bb9560e
Milou Arts <70600687+MilouArts@users.noreply.github.com>
2022-02-01 10:04:59 -0400

    ordination ABCDom not applicable to inoculum

--- R/ordination.R
+++ R/ordination.R
@@ -45,7 +45,6 @@ sites.long1<-sites.long(ordi.plot1.1, env.data = df.area.planC[1:(M-1)])
 ordi.data$NMDS.PlanC <- sites.long1
 fact.PlanC<-factor(ordi.data$NMDS.PlanC$Treatment, levels = c("Non-bleached + Ambient", "Non-bleached + Heated", "Bleached + Ambient", 'Bleached + Heated', "Ambient Water Control", "Heated Water Control", "not applicable"))
 
-
 ggplot() +
   geom_vline(xintercept = c(0), color = "grey70", linetype = 2) +
   geom_hline(yintercept = c(0), color = "grey70", linetype = 2) +
@@ -95,6 +94,7 @@ ordi.data$NMDS.ABCDom <- sites.long1
 cost.col.fill<-c("blue","red", "white", "white", "grey70", "grey70", "grey70")
 cost.col.line<-c("dodgerblue3", "firebrick3", "dodgerblue3", "firebrick3", "dodgerblue3", "firebrick3", "lightgrey")
 fact.all.treat<-factor(ordi.data$NMDS.ABCDom$Treatment, levels = c("Non-bleached + Ambient", "Non-bleached + Heated", "Bleached + Ambient", 'Bleached + Heated', "Ambient Water Control", "Heated Water Control", "not applicable"))
+treat.labels <- c("Non-bleached + Ambient", "Non-bleached + Heated", "Bleached + Ambient", 'Bleached + Heated', "Ambient Water Control", "Heated Water Control", "Inoculum")
 cost.shape <- c(21,24)
 
 ggplot() +
@@ -108,8 +108,8 @@ ggplot() +
              aes(x=axis1, y=axis2, fill = fact.all.treat, colour=fact.all.treat, shape = Timepoint_char),
              size=5, stroke=1.5) +
   scale_shape_manual(values = cost.shape, name = "Timepoint")+
-  scale_color_manual(values = cost.col.line, name = "Treatment")+
-  scale_fill_manual(values = cost.col.fill, name = "Treatment", guide = guide_legend(override.aes = list(shape = 21)))+
+  scale_color_manual(labels = treat.labels, values = cost.col.line, name = "Treatment")+
+  scale_fill_manual(labels = treat.labels, values = cost.col.fill, name = "Treatment", guide = guide_legend(override.aes = list(shape = 21)))+
   ggtitle("ABCDom")+
   coord_fixed(ratio=1)
 ggsave("NMDS_ABCDom.jpg", path = dirFigs, width = 6.75, height = 5, units = "in", dpi = 320)
@@ -120,6 +120,7 @@ ord.mod.area<- metaMDS(df.area.T0[M:ncol(df.area.T0)], distance = 'bray', k = 2)
 ordi.plot1.1<-ordiplot(ord.mod.area, choices = c(1,2))
 sites.long1<-sites.long(ordi.plot1.1, env.data = df.area.T0[1:(M-1)])
 ordi.data$NMDS.ABCDom.PlanC.T0 <- sites.long1
+
 cost.col.fill<-c("blue","red", "white", "white", "grey70", "grey70", "grey70")
 cost.col.line<-c("dodgerblue3", "firebrick3", "dodgerblue3", "firebrick3", "dodgerblue3", "firebrick3", "lightgrey")
 fact.all.treat<-factor(ordi.data$NMDS.ABCDom.PlanC.T0$Treatment, levels = c("Non-bleached + Ambient", "Non-bleached + Heated", "Bleached + Ambient", 'Bleached + Heated', "Ambient Water Control", "Heated Water Control", "not applicable"))
@@ -148,9 +149,11 @@ ord.mod.area<- metaMDS(df.area.ABCT0[M:ncol(df.area.ABCT0)], distance = 'bray', 
 ordi.plot1.1<-ordiplot(ord.mod.area, choices = c(1,2))
 sites.long1<-sites.long(ordi.plot1.1, env.data = df.area.ABCT0[1:(M-1)])
 ordi.data$NMDS.ABCDom.T0 <- sites.long1
+
 cost.col.fill<-c("blue","red", "white", "white", "grey70", "grey70", "grey70")
 cost.col.line<-c("dodgerblue3", "firebrick3", "dodgerblue3", "firebrick3", "dodgerblue3", "firebrick3", "lightgrey")
 fact.all.treat<-factor(ordi.data$NMDS.ABCDom.T0$Treatment, levels = c("Non-bleached + Ambient", "Non-bleached + Heated", "Bleached + Ambient", 'Bleached + Heated', "Ambient Water Control", "Heated Water Control", "not applicable"))
+treat.labels <- c("Non-bleached + Ambient", "Non-bleached + Heated", "Bleached + Ambient", 'Bleached + Heated', "Ambient Water Control", "Heated Water Control", "Inoculum")
 
 ggplot() +
   geom_vline(xintercept = c(0), color = "grey70", linetype = 2) +
@@ -162,8 +165,8 @@ ggplot() +
   geom_point(data=ordi.data$NMDS.ABCDom.T0,
              aes(x=axis1, y=axis2, fill = fact.all.treat, colour=fact.all.treat),
              size=5, stroke=1.5, shape = 21) +
-  scale_color_manual(values = cost.col.line, name = "Treatment")+
-  scale_fill_manual(values = cost.col.fill, name = "Treatment", guide = guide_legend(override.aes = list(shape = 21)))+
+  scale_color_manual(labels = treat.labels, values = cost.col.line, name = "Treatment")+
+  scale_fill_manual(labels = treat.labels, values = cost.col.fill, name = "Treatment", guide = guide_legend(override.aes = list(shape = 21)))+
   ggtitle("ABCDom T0")+
   coord_fixed(ratio=1)
 ggsave("NMDS_ABCDom_T0.jpg", path = dirFigs, width = 6.75, height = 5, units = "in", dpi = 320)
@@ -254,10 +257,12 @@ species.long2 <- species.long(ordi.plot2, spec.data=spec.data.envit)
 species.long3 <- species.long2[species.long2$r >= 0.9, ]
 ordi.data$Redun.ABCDom <- sites.long2
 ordi.data$drivers.redund.ABCDom <- species.long2
+
 cost.col.fill<-c("blue","red", "white", "white", "grey70", "grey70", "grey70")
 cost.col.line<-c("dodgerblue3", "firebrick3", "dodgerblue3", "firebrick3", "dodgerblue3", "firebrick3", "lightgrey")
 fact.all.treat<-factor(ordi.data$Redun.ABCDom$Treatment, levels = c("Non-bleached + Ambient", "Non-bleached + Heated", "Bleached + Ambient", 'Bleached + Heated', "Ambient Water Control", "Heated Water Control", "not applicable"))
 cost.shape <- c(21,24)
+treat.labels <- c("Non-bleached + Ambient", "Non-bleached + Heated", "Bleached + Ambient", 'Bleached + Heated', "Ambient Water Control", "Heated Water Control", "Inoculum")
 
 ggplot()+
   geom_vline(xintercept = c(0), color = "grey70", linetype = 2)+
@@ -270,10 +275,9 @@ ggplot()+
              size=5, stroke=1.5)+
   geom_segment(data=ordi.data$drivers.redund.ABCDom[ordi.data$drivers.redund.ABCDom$r >= 0.85,],
                aes(x=0, y=0, xend=axis1*4, yend=axis2*4), colour="red", size=0.7, arrow=arrow())+
-  #geom_text(data=species.long3, aes(x=axis1*4, y=axis2*4, label=labels),colour="red")+
   scale_shape_manual(values = cost.shape, name = "Timepoint")+
-  scale_color_manual(values = cost.col.line, name = "Treatment")+
-  scale_fill_manual(values = cost.col.fill, name = "Treatment", guide = guide_legend(override.aes = list(shape = 21)))+
+  scale_color_manual(labels = treat.labels, values = cost.col.line, name = "Treatment")+
+  scale_fill_manual(labels = treat.labels, values = cost.col.fill, name = "Treatment", guide = guide_legend(override.aes = list(shape = 21)))+
   ggtitle("ABCDom T0, r>0.85")+
   coord_fixed(ratio=2)
 ggsave("redundancy_ABCDom_Treatment_Timepoint.jpg", path = dirFigs, width = 6.75, height = 5, units = "in", dpi = 320)
@@ -362,6 +366,7 @@ species.long2 <- species.long(ordi.plot2, spec.data=spec.data.envit)
 species.long3 <- species.long2[species.long2$r >= 0.8, ]
 ordi.data$Redun.ABCDom.PlanC.T0 <- sites.long2
 ordi.data$drivers.redund.ABCDom.PlanC.T0 <- species.long2
+
 cost.col.fill<-c("blue","red", "white", "white", "grey70", "grey70", "grey70")
 cost.col.line<-c("dodgerblue3", "firebrick3", "dodgerblue3", "firebrick3", "dodgerblue3", "firebrick3", "lightgrey")
 fact.all.treat<-factor(ordi.data$Redun.ABCDom.PlanC.T0$Treatment, levels = c("Non-bleached + Ambient", "Non-bleached + Heated", "Bleached + Ambient", 'Bleached + Heated', "Ambient Water Control", "Heated Water Control", "not applicable"))
@@ -420,6 +425,8 @@ cost.col.fill<-c("blue","red", "white", "white", "grey70", "grey70", "grey70")
 cost.col.line<-c("dodgerblue3", "firebrick3", "dodgerblue3", "firebrick3", "dodgerblue3", "firebrick3", "lightgrey")
 fact.all.treat<-factor(ordi.data$Redun.ABCDom.T0$Treatment, levels = c("Non-bleached + Ambient", "Non-bleached + Heated", "Bleached + Ambient", 'Bleached + Heated', "Ambient Water Control", "Heated Water Control", "not applicable"))
 cost.shape <- c(21,24)
+treat.labels <- c("Non-bleached + Ambient", "Non-bleached + Heated", "Bleached + Ambient", 'Bleached + Heated', "Ambient Water Control", "Heated Water Control", "Inoculum")
+
 
 ggplot()+
   geom_vline(xintercept = c(0), color = "grey70", linetype = 2)+
@@ -434,8 +441,8 @@ ggplot()+
                aes(x=0, y=0, xend=axis1*4, yend=axis2*4), colour="red", size=0.7, arrow=arrow())+
   #geom_text(data=species.long3, aes(x=axis1*4, y=axis2*4, label=labels),colour="red")+
   scale_shape_manual(values = 21)+
-  scale_color_manual(values = cost.col.line, name = "Treatment")+
-  scale_fill_manual(values = cost.col.fill, name = "Treatment", guide = guide_legend(override.aes = list(shape = 21)))+
+  scale_color_manual(labels = treat.labels, values = cost.col.line, name = "Treatment")+
+  scale_fill_manual(labels = treat.labels, values = cost.col.fill, name = "Treatment", guide = guide_legend(override.aes = list(shape = 21)))+
   ggtitle("ABCDom T0, r>0.8")+
   coord_fixed(ratio=1)
 ggsave("redundancy_ABCDom_T0_Treatment.jpg", path = dirFigs, width = 6.75, height = 5, units = "in", dpi = 320)
